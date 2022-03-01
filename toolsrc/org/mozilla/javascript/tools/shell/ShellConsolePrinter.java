95390190df74c6b50a83b7fdad666ffee39bae6d
MaxisTekfield <89495620+MaxisTekfield@users.noreply.github.com>
2022-03-01 21:01:12 +0800

    Update ShellConsolePrinter.java

--- toolsrc/org/mozilla/javascript/tools/shell/ShellConsolePrinter.java
+++ toolsrc/org/mozilla/javascript/tools/shell/ShellConsolePrinter.java
@@ -29,7 +29,7 @@ class ShellConsolePrinter implements NativeConsole.ConsolePrinter {
 
             if (stack != null) {
                 for (ScriptStackElement element : stack) {
-                    shellConsole.println(element.toString());
+                    console.println(element.toString());
                 }
             }
         } catch (IOException e) {
